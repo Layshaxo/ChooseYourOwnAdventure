3187ecf2c5a0d2cdb00840a006b2b2ef5390f27b
laysha castillo <laysha1219@gmail.com>
2018-03-27 21:43:26 +0000

    Your message here

--- js/game.js
+++ js/game.js
@@ -6,41 +6,87 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You come across a dark and creepy home on Halloween",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Enter the House",
+                    nextLevel: "House",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Keep going past the house",
+                    nextLevel: "ghost",
                 },
             ]
         },
 
-        cave: {
+        House: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You find some gold!!",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "keep going",
+                    nextLevel: "Food",
                 },
+                {
+                
+                    text: "Go back outside",
+                    nextLevel: "Ghost",
+                },
+                
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+
+        Food: {
+            message:"look at all this food! now will you keep going?",
             choices: [
-                {
-                    text: "Start over",
-                    nextLevel: "start",
+            { text:"keep going?",
+            nextlevel: "Die",
+        },
+        {
+            text: "eat too much food",
+            nextLevel: "Die",
+        },
+        
+        {
+            text: "run outside",
+            nextLevel: "Ghost",
+            
+            },
+]
+},
+
+candy: {
+            message: "You survived!!!!",
+            
+},
+
+Die: {
+            message: "Aww youu tried, but remember silly you can't survive!",
+            
                 },
+                
+            
+},
+
+        Ghost: {
+            message: "oh you thought you would get out so easy...you encounter a ghost!",
+            choices: [
+                
+                {
+                 text: "run back to the house",
+                    nextLevel: "house",
+                },
+                {
+                text: "fight it",
+                    nextLevel: "candy",
+                    
+                },
+                
             ]
         },
 
     }
-};
+;
